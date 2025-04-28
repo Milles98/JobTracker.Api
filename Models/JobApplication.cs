aed9f573948afd2572f15818b19c0055f909df12
Mille Elfver <mille.elfver98@gmail.com>
2025-04-28 09:22:28 +0200

    Add project files.

---
+++ Models/JobApplication.cs
@@ -0,0 +1,11 @@
+﻿namespace JobTracker.Api.Models;
+
+public class JobApplication
+{
+    public Guid Id { get; init; }
+    public string CompanyName { get; set; }
+    public string  Title { get; set; }
+    public DateTime ApplicationDate { get; set; }
+    public string Status { get; set; } = "Sent";
+    public string? Notes { get; set; }
+}
