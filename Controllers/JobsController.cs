d18b3f9a7e8c23cd417e3a6dfb537fb71b5ea6bf
Mille Elfver <mille.elfver98@gmail.com>
2025-04-28 10:47:17 +0200

    Added db connection, added controller and model

---
+++ Controllers/JobsController.cs
@@ -0,0 +1,26 @@
+﻿using JobTracker.Api.Data;
+using JobTracker.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTracker.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class JobsController(AppDbContext db) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<JobApplication>>> GetAll()
+    {
+        var jobs = await db.JobApplications.ToListAsync();
+        return Ok(jobs);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<JobApplication>> GetById(Guid id)
+    {
+        var job = await db.JobApplications.FindAsync(id);
+        if (job is null) return NotFound();
+        return Ok(job);
+    }
+}
---
+++ Migrations/20250428084008_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobTracker.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobApplications",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplicationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobApplications", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobApplications");
+        }
+    }
+}
